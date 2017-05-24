6c119368ccabbebaea58165383759b8fdb302cc0
Viewreful2 <somthing2435@gmail.com>
2017-05-24 20:55:08 +0900

    Update 20120000.py

--- 20120000.py
+++ 20120000.py
@@ -11,7 +11,7 @@ for text_case in range(t) :
                 if(arr[i][j] == num[k]) :
                     arr[i][j] = 0
                     flag = True
-                    break
+                    breakfdsafdsafsafdsa
             if(flag == True) :
                 break
         if(flag == True ) :
